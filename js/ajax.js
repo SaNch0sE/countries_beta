97770ae0030f9e34eef2e4a2099569db4a0e0eea
SaNch0sE <22202508+SaNch0sE@users.noreply.github.com>
2019-12-15 00:21:28 +0200

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,5 @@
 async function ajax(action, data, callback){
-	const url = 'server.php';
+	const url = 'http://aboyko.shpp.me/countries_beta/server.php';
 	let dataString = {
 		action: action,
 		data: data
