6edfac9e8ecfa8411c8e19f49ef5ef958ebc79b4
SaNch0sE <22202508+SaNch0sE@users.noreply.github.com>
2019-12-15 00:50:05 +0200

    Update server.php

--- server.php
+++ server.php
@@ -12,7 +12,7 @@ function savScore($connect, $name, $score)
 }
 function getScores($connect)
 {
-	$sql = 'SELECT name, score FROM Scores';
+	$sql = 'select top 10 * from Scores order by id desc';
     if ($data = $connect->query($sql)) {
 		$dbdata = array();
 		if ($data->num_rows > 0) {
